65ebbbbab3ad2848d4671f7bec5daef8cc3c626c
Hector Aristy <hrafaelaristy@gmail.com>
2026-03-20 20:19:55 -0400

    feat: Remove proxy and use import meta approach from vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-const apiTarget = process.env.VITE_API_TARGET || "http://localhost:8080";
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -14,11 +12,5 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    proxy: {
-      "/api": {
-        target: apiTarget,
-        changeOrigin: true,
-      },
-    },
   },
 });
